3fe67506a63294c871f36fee04652ee0ba1e7704
Afaq Ahmad <afaqmfaqqi@gmail.com>
2026-09-15 22:58:10 +0500

    Added Popup menue button and List tyle widget

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -18,28 +18,51 @@ class _HomeScreenState extends State<HomeScreen> {
           backgroundColor: Colors.teal,
           title: Text('Whatsapp', style: TextStyle(color: Colors.white)),
           bottom: TabBar(
-            labelColor: Colors.white, 
-            unselectedLabelColor: Colors.white70, 
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white70,
             indicatorColor: Colors.white,
             tabs: [
-            Icon(Icons.camera_alt),
-            Text('Chats'),
-            Text('Status'),
-            Text('Calls'),
-          ]
+              Tab(child: Icon(Icons.camera_alt)),
+              Tab(child: Text('Chats')),
+              Tab(child: Text('Status')),
+              Tab(child: Text('Calls')),
+            ],
           ),
           actions: [
             Icon(Icons.search),
-            SizedBox(width: 10,)
+            SizedBox(width: 10),
+            PopupMenuButton(
+              icon: Icon(Icons.more_vert_outlined),
+              itemBuilder: (context) => [
+                PopupMenuItem(value: 1, child: Text('New Group')),
+                PopupMenuItem(value: 2, child: Text('Settings')),
+                PopupMenuItem(value: 3, child: Text('Log out')),
+              ],
+            ),
+            SizedBox(width: 10),
           ],
         ),
         body: TabBarView(
           children: [
             Text('Camera'),
-            Text('Chats'),
+            ListView.builder(
+              itemCount: 15,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      'https://cdn.britannica.com/67/126567-050-A5C3A312/Paulo-Coelho-departure-themes-thriller-serial-killer-2008.jpg'
+                    ),
+                  ),
+                  title: Text('Paulo Coelho'),
+                  subtitle: Text('65 million copies sold...Hurrayyy!!!'),
+                  trailing: Text('10:48 PM'),
+                );
+              }),
+            ),
             Text('Status'),
-            Text('Calls')
-          ]
+            Text('Calls'),
+          ],
         ),
       ),
     );
